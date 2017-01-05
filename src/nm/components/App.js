3b7a955e2a45f5a68ef494818e38ece1a6a2a543
DengrongGuan12 <1031212913@qq.com>
2017-01-05 11:53:46 +0800

    promise

--- src/nm/components/App.js
+++ src/nm/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import ServiceClient from "../../base/service/ServiceClient";
+import PlayList from "./PlayList";
 export default class App extends Component {
 
     constructor (props) {
@@ -15,18 +17,40 @@ export default class App extends Component {
     }
 
     state = {
-
+      playlists: []
     }
-
-    componentDidMount()
+    async componentDidMount()
     {
-
+      const result = await ServiceClient.getInstance().getAsyncUserPlayLists("77464772");
+      // console.log(result);
+      this.setState({
+        playlists: result
+      });
+      // ServiceClient.getInstance().getUserPlayLists("77464772").then(res => {
+      //   return res;
+      // }).catch(error =>{
+      //   console.log(error);
+      // }).then(res => {
+      //   console.log(res);
+      // });
     }
 
     render()
     {
+      const {playlists} = this.state;
+      console.log(playlists);
       return (
-        <div className="nm-app">Netease Music</div>
+        <div className="nm-app">
+          <header>
+            <div className="logo"></div>
+            <h1>网易云音乐</h1>
+          </header>
+          <main>
+            <aside><PlayList playlists={playlists}/></aside>
+            <section></section>
+          </main>
+          <footer></footer>
+        </div>
       );
 
     }
